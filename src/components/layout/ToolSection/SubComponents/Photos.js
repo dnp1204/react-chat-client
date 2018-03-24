23e29f41a93ca7e7056ff4956800893663ecc617
dnp <doanhpham1995@gmail.com>
2018-03-23 21:15:00 -0400

    change text

--- src/components/layout/ToolSection/SubComponents/Photos.js
+++ src/components/layout/ToolSection/SubComponents/Photos.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 import BaseComponent from './BaseComponent';
 
 class Photos extends Component {
-  
-  state = { showChild: true }
-  
+  state = { showChild: true };
+
   render() {
     return (
-      <BaseComponent componentName="Photos" classNameToCustom="tool--photos">
-        <div>Hello</div>
+      <BaseComponent
+        componentName="Shared Photos"
+        classNameToCustom="tool--photos"
+      >
+        <div>...</div>
       </BaseComponent>
     );
   }

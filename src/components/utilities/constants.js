7dbd399db0b77cebd8b8aeafdd04458380c18fe1
dnp <doanhpham1995@gmail.com>
2018-03-23 22:52:37 -0400

    ADD SOME COLOR

--- src/components/utilities/constants.js
+++ src/components/utilities/constants.js
@@ -1,7 +1,10 @@
 exports.Color = {
-  BLUE: '#0584FF',
   BLACK_OPACITY_04: 'rgba(0, 0, 0, 0.4)',
   BLACK_OPACITY_03: 'rgba(0, 0, 0, 0.3)',
   BLACK_OPACITY_02: 'rgba(0, 0, 0, 0.2)',
-  LIGHT_WHITE: '#F1F0F0'
+  BLUE: '#0584FF',
+  LIGHT_WHITE: '#F1F0F0',
+  PELOROUS: '#45BEC7',
+  AMBER: '#FFC302',
+  CORAL_RED: '#FA3C4C'
 };

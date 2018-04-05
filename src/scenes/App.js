0b8a268cf1cb4a64ac06882e106f3154a904fc74
Doanh Pham <dnp4736@rit.edu>
2018-04-05 17:53:58 +0000

    added login and signup component

--- src/scenes/App.js
+++ src/scenes/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import MainScene from './main/MainScene';
+import Login from './login/Login';
 
 class App extends Component {
   render() {
     return (
-      <MainScene />
+      <Login />
     );
   }
 }
---
+++ src/scenes/login/Login.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './Login.scss';
+
+class Login extends Component {
+    render() {
+        return (
+            <div className={`app-login-page ${this.props.hidden}`}>
+                <div className="login-page-container">
+                    <h1 className="logo">React Messenger</h1>
+                    <a
+                    href="/auth/google"
+                    className="btn btn-block btn-danger"
+                    id="google-login-link"
+                    >
+                        <i className="fa fa-google" /> Log in with Google
+                    </a>
+                    <a
+                    href="/auth/facebook"
+                    className="btn btn-block btn-primary"
+                    id="facebook-login-link"
+                    >
+                        <i className="fa fa-facebook" /> Log in with Facebook
+                    </a>
+                    <div className="split-line-container">
+                        <div className="split-line" />
+                        <div className="between">OR</div>
+                        <div className="split-line" />
+                    </div>
+                    <div className="form-group">
+                        <input className="form-control" placeholder="Email" />
+                        <input className="form-control" placeholder="First Name" />
+                        <input className="form-control" placeholder="Last Name" />
+                        <input className="form-control" placeholder="Password" />
+                        <input className="form-control" placeholder="Password Again" />
+                    </div>
+                    <button className="btn btn-block btn-primary">Sign Up</button>
+                </div>
+            </div> 
+        );
+    }
+}
+
+export default Login;

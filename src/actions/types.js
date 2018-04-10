89e834925670164040621f2f5636e544e4ca5a9a
Doanh Pham <dnp4736@rit.edu>
2018-04-10 19:59:11 +0000

    remove a file

--- src/actions/types.js
+++ src/actions/types.js
@@ -7,8 +7,8 @@ export const FETCH_MESSAGES = 'fetch_messages';
 export const SEND_MESSAGE = 'send_message';
 
 export const SELECT_EMOJI = 'select_emoji';
-export const FETCH_RECENTLY_USED_EMOJI = 'fetch_recently_used_emoji';
 
+export const FETCH_RECENTLY_USED_EMOJI = 'fetch_recently_used_emoji';
 export const FETCH_SYSTEM_SETTINGS = 'fetch_system_settings';
 export const CHANGE_SYSTEM_COLOR = 'change_system_color';
 export const CHANGE_SHOW_OPTIONS = 'change_show_options';

7107638dc04313cc674e84adbf0419c94bed050b
Doanh Pham <doanhpham1995@gmail.com>
2018-05-23 22:42:44 -0400

    move stuff

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-
-const app = express();
-app.use(morgan("combined"));
-app.use(bodyParser.json({ type: "*/*" }));
-
-module.exports = app;

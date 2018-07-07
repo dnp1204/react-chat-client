f40c426ea2b85348dbd502183fd0f29eb32f0da6
Doanh Pham <doanhpham1995@gmail.com>
2018-07-06 22:44:03 -0400

    start app

--- _client/src/index.js
+++ _client/src/index.js
@@ -1,32 +1,39 @@
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import './styles/index.scss';
+import 'emoji-mart/css/emoji-mart.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore, compose } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import reduxThunk from 'redux-thunk';
+
 import reducers from './reducers';
-import App from './scenes/App';
-import './styles/index.scss';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
-import 'emoji-mart/css/emoji-mart.css'
 import registerServiceWorker from './registerServiceWorker';
+import App from './scenes/App';
 
 let middlewares = [];
 if (process.env.NODE_ENV === `development`) {
   const { createLogger } = require(`redux-logger`);
-  
+
   const reduxLogger = createLogger({
     level: 'log',
     collapsed: true
   });
-  
+
   middlewares.push(reduxLogger);
 }
 
-const store = createStore(reducers, {}, compose(applyMiddleware(reduxThunk, ...middlewares)));
+const store = createStore(
+  reducers,
+  {},
+  compose(applyMiddleware(reduxThunk, ...middlewares))
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>, document.getElementById('root')
+  </Provider>,
+  document.getElementById('root')
 );
 registerServiceWorker();

47f8cca94d832cd47c4a9338ecb88207c347fbaa
Doanh Pham <doanhpham1995@gmail.com>
2018-08-03 00:14:52 -0400

    fix deploy

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ chat(app, io, session);
 app.use(handleError);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('_client/build'));
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '_client', 'build', 'index.html'));
   });
 }
 

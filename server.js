b31aebcd49d14f03e78a9ad3ad78f06da0992cfc
Doanh Pham <doanhpham1995@gmail.com>
2018-08-03 00:03:24 -0400

    add

--- server.js
+++ server.js
@@ -27,7 +27,13 @@ chat(app, io, session);
 
 app.use(handleError);
 
-// io.of('/').on('connection', socket => {});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 server.listen(app.get('port'), err => {
   if (err) {

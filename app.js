a15469028384f9f7d9675069e7fa2aa58cce2a6b
Doanh Pham <doanhpham1995@gmail.com>
2018-08-03 00:24:19 -0400

    anotehr fix

--- app.js
+++ app.js
@@ -49,7 +49,9 @@ const io = socket(server);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('_client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '_client', 'build', 'index.html'));
+    res.sendFile(
+      path.resolve(process.env.PWD, '_client', 'build', 'index.html')
+    );
   });
 }
 

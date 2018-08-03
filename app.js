acb23cb737f7080d49d28686129e8233f9a6ba56
Doanh Pham <doanhpham1995@gmail.com>
2018-08-03 00:57:10 -0400

    fixed

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const io = socket(server);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('_client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve('./', '_client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '_client', 'build', 'index.html'));
   });
 }
 
